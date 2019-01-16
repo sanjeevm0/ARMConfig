7769ef1655a8782b1756417d036c3435f3fde7bd
sanjeevm0 <sanjeevm@microsoft.com>
2019-01-16 11:43:52 -0800

    update

--- configrepeat.sh
+++ configrepeat.sh
@@ -1,12 +1,13 @@
 #!/bin/bash 
 
-#sed '0,/config.sh/{s/config.sh/configrepeat.sh/}' testrepeat.sh > testrepeat_temp.sh; mv testrepeat_temp.sh testepeat.sh
+#sed '0,/config.sh/{s/config.sh/configrepeat.sh/}' testrepeat.sh > testrepeat_temp.sh; mv testrepeat_temp.sh testrepeat.sh
 cp testrepeat.sh configold.sh
 
 printf "bash configrepeat.sh" > testrepeat.sh
 for var in "$@"
 do
-    printf ' "' >> $testrepeat.sh
-    echo -n $var >> $testrepeat.sh
-    printf '"' >> $testrepeat.sh
+    printf " '" >> testrepeat.sh
+    echo -n $var >> testrepeat.sh
+    printf "'" >> testrepeat.sh
 done
+
